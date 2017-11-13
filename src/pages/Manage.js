30e820df33027bf381bfd14ce38e742b7165fce8
zhangxd <zhangxd@knownsec.com>
2017-11-13 21:00:38 +0800

    增加分类代码

---
+++ src/pages/Manage.js
@@ -0,0 +1,43 @@
+import React, {Component} from "react";
+import Category from "./Category";
+import UploadImg from "./components/Upload";
+import {Switch, Route, Link} from "react-router-dom";
+import {Row, Col, Menu} from "antd";
+import "../css/manage.css";
+
+class Manage extends Component {
+  render() {
+	return(
+	  <div className="manage">
+	  <Row>
+	  <Col md={4} xs={8} sm={6} className="nav">
+	  <h1>这里是管理中心</h1>
+	  <hr style={{margin: 10}}/>
+	  <div className="manage">
+	  <h2>管理菜单</h2>
+	  <Menu>
+	  <Menu.Item>
+	  <Link to="/manage/category">增加分类</Link>
+	  </Menu.Item>
+	  <Menu.Item>
+	  <Link to="/manage/product">增加产品</Link>
+	  </Menu.Item>
+	  <Menu.Item>
+	  <Link to="/manage/upload">上传图片</Link>
+	  </Menu.Item>
+	  </Menu>
+	  </div>
+	  </Col>
+	  <Col md={20} xs={16} sm={6}>
+	  <Switch>
+	  <Route path="/manage/category" component={Category}/>
+	  <Route path="/manage/upload" component={UploadImg}/>
+	  </Switch>
+	  </Col>
+	  </Row>
+	  </div>
+	)
+  }
+}
+
+export default Manage;
---
+++ src/pages/components/Upload.js
@@ -0,0 +1,52 @@
+import React, {Component} from "react";
+import { Upload, Icon, Modal } from 'antd';
+
+class UploadImg extends Component {
+  state = {
+    previewVisible: false,
+    previewImage: '',
+    fileList: [],
+  };
+
+  handleCancel = () => this.setState({ previewVisible: false })
+
+  handlePreview = (file) => {
+    this.setState({
+      previewImage: file.url || file.thumbUrl,
+      previewVisible: true,
+    });
+  }
+
+  handleChange({ fileList }) {
+    console.log(fileList);
+    this.setState({ fileList });
+  }
+
+  render() {
+    const { previewVisible, previewImage, fileList } = this.state;
+    const uploadButton = (
+      <div>
+        <Icon type="plus" />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    );
+    return (
+      <div className="clearfix">
+        <Upload
+          action="http://localhost:3000/upload"
+          listType="picture-card"
+          fileList={fileList}
+          onPreview={this.handlePreview}
+          onChange={this.handleChange.bind(this)}
+        >
+          {fileList.length >= 8 ? null : uploadButton}
+        </Upload>
+        <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+          <img alt="example" style={{ width: '100%' }} src={previewImage} />
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default UploadImg;
